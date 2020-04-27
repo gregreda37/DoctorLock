6357af4467b39d1e907262d914603cffcdfed5f5
Greg Reda <54119966+gregreda37@users.noreply.github.com>
2020-04-27 13:37:22 -0400

    21 objects

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,10 @@
+import 'package:doc_lock/models/brew.dart';
+import 'package:doc_lock/services/database.dart';
 import 'package:flutter/material.dart';
 import 'package:doc_lock/services/auth.dart';
+import 'package:provider/provider.dart';
+import 'package:doc_lock/screens/home/brew_list.dart';
+
 //stles to create widget
 class Home extends StatelessWidget {
 
@@ -7,21 +12,40 @@ final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        title: Text('Greg'),
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label:Text('logout'),
-            onPressed: () async {
-              await _auth.signOut();
-            },
-          )
-        ],
+    void _showSettingsPanel() {
+      showModalBottomSheet(context: context, builder: (context){
+        return Container(
+          padding: EdgeInsets.symmetric(vertical:20.0, horizontal: 60.0),
+          child: Text("Bottom sheet"),
+        );
+      });
+    }
+
+
+    return StreamProvider<List <Brew>>.value(
+        value: DatabaseService().brews, 
+        child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: Text('Greg'),
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.person),
+              label:Text('logout'),
+              onPressed: () async {
+                await _auth.signOut();
+              },
+            ),
+            FlatButton.icon(
+              icon: Icon(Icons.settings),
+              label: Text("settings"),
+              onPressed: () => _showSettingsPanel(),
+            ),
+          ],
+        ),
+        body: BrewList(),
       ),
     );
   }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:doc_lock/models/brew.dart';
 
 class DatabaseService {
 
@@ -13,5 +14,21 @@ class DatabaseService {
       "name": name,
       "strength": strength
     });
+  }
+  //brew list from snapshot
+  List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot){
+    return snapshot.documents.map((doc){
+      return Brew(
+        name: doc.data["name"] ?? "",
+        strength: doc.data["strength"] ?? 0,
+        sugars: doc.data["sugars"] ?? "0"
+      );
+    }).toList();
+  }
+
+
+  // get stream
+  Stream<List<Brew>> get brews {
+    return brewCollection.snapshots().map(_brewListFromSnapshot);
   }
 }
